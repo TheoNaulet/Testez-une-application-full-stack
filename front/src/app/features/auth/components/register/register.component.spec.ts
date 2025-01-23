0bb08eec82f5d7d9fe88691642f1a36672db5fc3
Naulet Théo <theo.naulet@gmail.com>
2025-01-23 09:38:26 +0100

    feat(test): add register component test

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,6 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { of, throwError } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -37,4 +40,53 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create an account successfully and redirect to login', () => {
+    const authService = TestBed.inject(AuthService);
+    const router = TestBed.inject(Router);
+  
+    jest.spyOn(authService, 'register').mockReturnValue(of(undefined));
+  
+    jest.spyOn(router, 'navigate').mockImplementation(() => Promise.resolve(true));
+  
+    component.form.controls['email'].setValue('test@example.com');
+    component.form.controls['password'].setValue('password123');
+    component.form.controls['firstName'].setValue('Test');
+    component.form.controls['lastName'].setValue('User');
+  
+    component.submit();
+  
+    expect(authService.register).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password123',
+      firstName: 'Test',
+      lastName: 'User'
+    });
+  
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+
+  it('should display an error message if account creation fails', () => {
+    const authService = TestBed.inject(AuthService);
+  
+    jest.spyOn(authService, 'register').mockReturnValue(
+      throwError(() => new Error('Email already exists'))
+    );
+  
+
+    component.form.controls['email'].setValue('existing@example.com');
+    component.form.controls['password'].setValue('password123');
+    component.form.controls['firstName'].setValue('Test');
+    component.form.controls['lastName'].setValue('User');
+  
+
+    component.submit();
+    fixture.detectChanges();
+  
+
+    const errorMessage = fixture.nativeElement.querySelector('.error');
+    expect(errorMessage).toBeTruthy(); 
+    expect(errorMessage.textContent).toContain('An error occurred'); 
+  });
+  
 });
