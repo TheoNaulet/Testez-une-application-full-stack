313890864eae8c88d9b436adb91f547cd5a7a108
Naulet Théo <theo.naulet@gmail.com>
2025-01-23 09:38:43 +0100

    feat(test): add me component test

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,6 +11,13 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+
+
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -38,5 +45,50 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable the submit button if the form is invalid', () => {
+    const form = component.form;
+  
+    expect(form.valid).toBeFalsy();
+  
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    expect(submitButton.disabled).toBeTruthy(); 
+  });
+
+  it('should log in successfully with valid credentials', () => {
+    const authService = TestBed.inject(AuthService);
+    const sessionService = TestBed.inject(SessionService);
+    const router = TestBed.inject(Router);
+  
+    jest.spyOn(router, 'navigate').mockImplementation(() => Promise.resolve(true));
+  
+    const mockSessionInformation: SessionInformation = {
+      token: 'fake-jwt-token',
+      type: 'Bearer',
+      id: 1,
+      username: 'testuser',
+      firstName: 'Test',
+      lastName: 'User',
+      admin: false
+    };
+  
+    jest.spyOn(authService, 'login').mockReturnValue(of(mockSessionInformation)); 
+    jest.spyOn(sessionService, 'logIn'); 
+  
+    component.form.controls['email'].setValue('test@example.com');
+    component.form.controls['password'].setValue('password123');
+  
+    // Appelle la méthode submit
+    component.submit();
+  
+    expect(authService.login).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'password123'
+    });
+  
+    expect(sessionService.logIn).toHaveBeenCalledWith(mockSessionInformation);
+  
+    expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
   });
 });
