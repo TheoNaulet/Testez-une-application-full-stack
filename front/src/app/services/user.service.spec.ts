1d07cc8a8233331e7b516f1c149ef5703f3f661b
Naulet Théo <theo.naulet@gmail.com>
2025-02-06 17:12:52 +0100

    test(user): add tests to user service

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
@@ -6,17 +6,43 @@ import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
     });
+
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('delete()', () => {
+    it('should send a DELETE request to the correct URL', () => {
+      // Arrange
+      const userId = '1';
+
+      // Act
+      service.delete(userId).subscribe(response => {
+        // Vérifier que la réponse est bien reçue
+        expect(response).toBeTruthy();
+      });
+
+      // Assert
+      const req = httpTestingController.expectOne(`api/user/${userId}`);
+      expect(req.request.method).toBe('DELETE');
+
+      // Simuler une réponse HTTP réussie
+      req.flush({});
+    });
   });
 });
