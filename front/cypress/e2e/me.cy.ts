d165711830b3cce6a772c81dce554cec240e7a20
Naulet Théo <theo.naulet@gmail.com>
2025-02-20 11:29:17 +0100

    chore(tests): add me cy tests

--- front/cypress/e2e/me.cy.ts
+++ front/cypress/e2e/me.cy.ts
@@ -1,6 +1,5 @@
 describe('User e2e me test', () => {
   it('Me', () => {
-
     let sessionUsers: Number[] = []; // Initialize an empty array for session users
 
     // Log in as a user with email and password
@@ -22,7 +21,6 @@ describe('User e2e me test', () => {
         password: "password", // Mocked password (should not be stored like this in production)
         createdAt: new Date(),
         updatedAt: new Date()
-
       },
     ).as('user')
 
@@ -78,4 +76,39 @@ describe('User e2e me test', () => {
     // Verify that the URL changes to the sessions page
     cy.url().should('include', '/sessions')
   })
+
+  it('should delete the user account', () => {
+    // Log in as a user
+    cy.login('yoga@studio.com', 'test!1234');
+
+    // Intercept the API call to fetch user details
+    cy.intercept('GET', '/api/user/1', {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: "yoga@studio.com",
+      admin: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }).as('user');
+
+    // Intercept the DELETE request to simulate account deletion
+    cy.intercept('DELETE', '/api/user/1', {}).as('deleteUser');
+
+    // Navigate to the "Me" page
+    cy.get('span[routerLink=me]').click();
+    cy.url().should('include', '/me');
+
+    // Verify the delete button is visible and click it
+    cy.get('button').contains('Detail').click();
+
+    // Wait for the DELETE request and confirm it was called
+    cy.wait('@deleteUser').its('request.method').should('eq', 'DELETE');
+
+    // Check if the snack-bar appears with the correct message
+    cy.get('.mat-snack-bar-container').should('contain.text', 'Your account has been deleted !');
+
+    // Verify that the user is redirected to the home page
+    cy.url().should('eq', Cypress.config().baseUrl);
+  });
 });
